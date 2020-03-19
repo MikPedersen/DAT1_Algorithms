8d81c40ced4e11d2403d76a1b6247d1b57c45e7c
Mik Pedersen <45688364+MikPedersen@users.noreply.github.com>
2020-03-19 15:44:04 +0100

    implementeret MyTimer, så jeg endelig kan se forskel

--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,7 @@ import java.util.Date;
 public class Main {
     static long startTime = MyTimer.currentTimestamp();
     static long endTime = MyTimer.currentTimestamp();
-    static Date date=new Date(startTime);
+    static Date date= new Date(startTime);
     static String resultat = MyTimer.timeElapsed(startTime, endTime) ;
     private static final int N = 70000; // Change the value of N for more or less iterations
 
@@ -51,20 +51,26 @@ public class Main {
         for (int i = 0; i < N; i++) {
             Algoritmer.data.add((int) (Math.random() * 250001));
         }
+        MyTimer.start();
         Algoritmer.selectionSort();
         System.out.println();
-        System.out.println(resultat);
+        MyTimer.stop();
+        MyTimer.timeElapsed(startTime, endTime);
+//        System.out.println(resultat);
 
     }
     private static void myBubbleSort(){
         for (int i = 0; i < N; i++) {
             Algoritmer.data.add((int) (Math.random() * 250001));
         }
-        System.out.println("Time is " + date.toString() + " (" + date.getTime() + ")\n");
+        MyTimer.start();
+//        System.out.println("Time is " + date.toString() + " (" + date.getTime() + ")\n");
         System.out.println("Calculation starting.");
         Algoritmer.bubbleSort(Algoritmer.data);
         System.out.println();
-        System.out.println(resultat);
+        MyTimer.stop();
+        MyTimer.timeElapsed(startTime, endTime);
+//        System.out.println(resultat);
     }
     private static void myFindMax(){
         System.out.println("Time is " + date.toString() + " (" + date.getTime() + ")\n");
